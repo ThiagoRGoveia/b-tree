66477055f3a356434f75cbe1efd7fde2b124e988
Thiago <t.r.goveia@gmail.com>
2021-05-29 14:49:45 -0300

    Adiciona constantes com tamanhos dos vetores de páginas filhas e de chaves primárias e com nomes dos arquivos de páginas

--- Defines.h
+++ Defines.h
@@ -2,7 +2,11 @@
 #define DEFINES_H_
 
 #define B_TREE_ORDER 8
-#define PAGE_SIZE 8 + 4 * (2*B_TREE_ORDER - 1)
+#define PAGE_SIZE 8 + 4 * (2 * B_TREE_ORDER - 1)
 #define STRING_FIELD_SIZE 255
 #define STUDENT_SIZE 8 + STRING_FIELD_SIZE * 3
+#define PAGE_CHILDREN_VECTOR_SIZE 4 * B_TREE_ORDER
+#define KEYS_VECTOR_SIZE 4 * (B_TREE_ORDER - 1)
+#define INDEX_FILE "pages.dat"
+#define DATA_FILE "data.dat"
 #endif
